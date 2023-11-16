41099d6e7ebcae4ed6b5976cccff06a624df46ef
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-17 01:52:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	open_file(argv[1]);
+	ofile(argv[1]);
 	free_nodes();
 	return (0);
 }
@@ -31,7 +31,7 @@ stack_t *create_node(int n)
 
 	node = malloc(sizeof(stack_t));
 	if (node == NULL)
-		err(4);
+		_err(4);
 	node->next = NULL;
 	node->prev = NULL;
 	node->n = n;
