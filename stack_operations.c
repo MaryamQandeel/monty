cd744e5ef562b10ccc3c097b9a61489d80d1a3a5
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-17 01:23:40 +0200

    Create stack_operations.c

---
+++ stack_operations.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * _mul - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @l_num: Interger representing the line number of of the opcode.
+ */
+void _mul(stack_t **stack, unsigned int l_num)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, l_num, "mul");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+
+/**
+ * mod_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @l_num: Interger representing the line number of of the opcode.
+ */
+void _mod(stack_t **stack, unsigned int l_num)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, l_num, "mod");
+
+
+	if ((*stack)->n == 0)
+		more_err(9, l_num);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
